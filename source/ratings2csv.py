a6542049bf1b7c69dd2cea7f5bc4d80959365229
kyle-kelly <kkelly.667@gmail.com>
2017-05-26 14:38:42 -0400

    This successfully scrapes titles and ratings from IMDB. Now need to scrape Metacritic

--- source/ratings2csv.py
+++ source/ratings2csv.py
@@ -1,101 +1,51 @@
-#!/usr/local/bin/python python
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#!/usr/local/bin/python
+
+"""
+This program scrapes ratings from a number of IMDB webpages. It also pulls ratings for 
+the corresponding movie from numerous reviewers on Metacritic. These are all written out
+to the CSV file ratings_data.csv in the /data directory.
+"""
+
+from lxml import html
+import requests
 import numpy
 import csv
 
-
-def write_out_csv(ratings):
-	with open("../data/ratings_data.csv", "wb") as f:
-		writer = csv.writer(f, delimiter = ',')
-		for j in range(len(ratings)):
-		    ratings[j] = map(float, ratings[j])
-		writer.writerows(ratings)
-
-
-def get_reviewer_ratings(driver, urls, ratings, reviewers):
-	"""Navigate to Metacritic and pull reviewer ratings"""
-
-	for iteration in range(len(urls)):
-
-		url = urls[iteration]
-		driver.get(url)
-		xpath_titles = "//h3[@class='lister-item-header']/a"
-		titleElements = driver.find_elements_by_xpath(xpath_titles)
-
-		for j in range(0,len(titleElements)):
-			driver.get(url)
-			titleElements = driver.find_elements_by_xpath(xpath_titles)
-			titleElements[j].click()
-			driver.find_element_by_class_name('titleReviewBarSubItem').click()
-			xpath = "//div[@class='see-more']/a"
-			urlElement = driver.find_element_by_xpath(xpath)
-			meta_url = urlElement.get_attribute("href")
-			print meta_url
-			chars = set('?')
-			if any((c in chars) for c in meta_url):
-				meta_url, trash = meta_url.split("?",1)			
-			meta_url = meta_url + "/critic-reviews"
-			print meta_url
-			driver.get(meta_url)
-
-			#List of all critic sources for a particular movie
-			xpath = "//span[@class='source']"
-			sourceElements = driver.find_elements_by_xpath(xpath)
-			n_sources = len(sourceElements)
-			print n_sources
-			
-			#List of all critic ratings for a particular movie
-			ratingElements = driver.find_elements_by_css_selector('.metascore_w.large.movie.indiv')
-			n_ratings = len(ratingElements)
-			print n_ratings
-			
-			#Check the number of sources match ratings and then add pertinent reviews to list
-			if n_sources == n_ratings:
-				for reviewer in reviewers:
-					for i in range(len(sourceElements)):
-						if reviewer == sourceElements[i].text:
-							ratings[50*iteration + j].append(ratingElements[i].text.encode('utf-8'))
-							break
-						else:
-							if i == len(sourceElements) - 1:
-								ratings[50*iteration + j].append('-1')
-				write_out_csv(ratings)
-			
-			else:
-				print "ERROR: Sources don't match ratings!!"
-
-	return ratings
-
-
-def get_title_IMDB_ratings(driver, urls, ratings, reviewers):
+def get_title_IMDB_ratings(urls, ratings, reviewers):
 	"""Pull title and IMDB ratings"""
 
 	titles = []
 	xpath_titles = "//h3[@class='lister-item-header']/a"
+	xpath_ratings =  "//div[@class='inline-block ratings-imdb-rating']/strong"
 
 	for i in range(len(urls)):
-		driver.get(urls[i])
-		titleElements = driver.find_elements_by_xpath(xpath_titles)
+		url = urls[i]
+		page = requests.get(url)
+		tree = html.fromstring(page.content)
+		
+		titleElements = tree.xpath(xpath_titles)
 		for element in titleElements:
 			titles.append(element.text)
-		ratingElements = driver.find_elements_by_class_name('ratings-imdb-rating')
+		
+		ratingElements = tree.xpath(xpath_ratings)
 		for element in range(len(ratingElements)):
-			ratings[50*i + element].append(ratingElements[element].get_attribute('data-value').encode('utf-8'))
+			ratings[50*i + element].append(ratingElements[element].text.encode('utf-8'))
 
-	print ratings
+	print titles, ratings
 	
 	return titles, ratings
 
 
 def main():
-
-	driver = webdriver.Chrome()
 	
-	urls = ("http://www.imdb.com/search/title?year=2016,2016&title_type=feature&sort=boxoffice_gross_us,desc",
+	urls = (
+		"http://www.imdb.com/search/title?year=2016,2016&title_type=feature&sort=boxoffice_gross_us,desc",
 		"http://www.imdb.com/search/title?year=2015,2015&title_type=feature&sort=boxoffice_gross_us,desc",
-		"http://www.imdb.com/search/title?year=2014,2014&title_type=feature&sort=boxoffice_gross_us,desc")
-	reviewers = ("Washington Post",
+		"http://www.imdb.com/search/title?year=2014,2014&title_type=feature&sort=boxoffice_gross_us,desc"
+		)
+	
+	reviewers = (
+		"Washington Post",
 		"The New York Times",
 		"The Telegraph",
 		"Los Angeles Times",
@@ -103,7 +53,8 @@ def main():
 		"Chicago Tribune",
 		"Boston Globe",
 		"Rolling Stone",
-		"RogerEbert.com")
+		"RogerEbert.com"
+		)
 	
 	titles_per_year = 50
 	n_total_titles = len(urls) * titles_per_year
@@ -111,11 +62,10 @@ def main():
 	ratings = []
 	for n in range(n_total_titles):
 		ratings.append([])
+	
 	titles =[]
 	
-	titles, ratings = get_title_IMDB_ratings(driver, urls, ratings, reviewers)
-	ratings = get_reviewer_ratings(driver, urls, ratings, reviewers)
-
+	titles, ratings = get_title_IMDB_ratings(urls, ratings, reviewers)
 
 if __name__ == "__main__":
     main()
